90b6f24796ad9d2c986a5f9dff590fcc8ec5b017
said017 <saidam017@gmail.com>
2022-09-25 19:44:28 +0700

    adjust 1155 metadata

--- contracts/MilestoneNFTv2.sol
+++ contracts/MilestoneNFTv2.sol
@@ -19,13 +19,16 @@ error NotOwner();
 error NotApprovedForMarketplace();
 error PriceMustBeAboveZero();
 
-contract MilestoneNFTv2 is ReentrancyGuard,ERC1155 {
+contract MilestoneNFTv2 is ReentrancyGuard, ERC1155 {
     using Counters for Counters.Counter;
 
     // ERC1155 data
 
     Counters.Counter private _tokenIds;
-    mapping(uint256 => string) private _tokenURIs;
+    string public name;
+    string public symbol;
+
+    mapping(uint256 => string) public tokenURI;
 
     // All structs
     struct Creator {
@@ -56,7 +59,8 @@ contract MilestoneNFTv2 is ReentrancyGuard,ERC1155 {
     );
 
     constructor() payable ERC1155("") {
-       
+        name = "Ciriverse";
+        symbol = "CIRI";
     }
 
     uint256 public constant MAX_MILESTONE = 5;
@@ -64,9 +68,6 @@ contract MilestoneNFTv2 is ReentrancyGuard,ERC1155 {
     mapping(address => Creator) private s_creators;
     mapping(address => Milestone[]) private s_milestoneArray;
     mapping(address => mapping(address => MilestonePerDonator)) s_donators;
-
-
-
 
     modifier notRegistered(address creatorAddress) {
         Creator memory creator = s_creators[creatorAddress];
@@ -100,21 +101,21 @@ contract MilestoneNFTv2 is ReentrancyGuard,ERC1155 {
     // register as a creator
     // TODO
     // check if user already exist?
-    function creatorRegister(string calldata name, string calldata pic)
+    function creatorRegister(string calldata _name, string calldata _pic)
         external
         notRegistered(msg.sender)
     {
-        s_creators[msg.sender] = Creator(msg.sender, name, pic, 0);
-        emit UserCreated(msg.sender, name, pic);
+        s_creators[msg.sender] = Creator(msg.sender, _name, _pic, 0);
+        emit UserCreated(msg.sender, _name, _pic);
     }
 
-    function updateProfile(string calldata name, string calldata pic)
+    function updateProfile(string calldata _name, string calldata _pic)
         external
         isRegistered(msg.sender)
     {
-        s_creators[msg.sender].name = name;
-        s_creators[msg.sender].pic = pic;
-        emit UserCreated(msg.sender, name, pic);
+        s_creators[msg.sender].name = _name;
+        s_creators[msg.sender].pic = _pic;
+        emit UserCreated(msg.sender, _name, _pic);
     }
 
     // gift creator a klay
@@ -137,10 +138,11 @@ contract MilestoneNFTv2 is ReentrancyGuard,ERC1155 {
                 // check if already marked true?
                 if (
                     (s_donators[msg.sender][creator].fund >=
-                        s_milestoneArray[creator][i].price) && s_donators[msg.sender][creator].minted[i] == false
+                        s_milestoneArray[creator][i].price) &&
+                    s_donators[msg.sender][creator].minted[i] == false
                 ) {
                     s_donators[msg.sender][creator].status[i] = true;
-                    console.log('jadi elligible?');
+                    console.log("jadi elligible?");
                     // check if donator already own?
                     // if not own and eligible, send the NFTs
                     // TODO comeback to this latter when we create NFT contract
@@ -156,8 +158,7 @@ contract MilestoneNFTv2 is ReentrancyGuard,ERC1155 {
         uint256 price
     )
         internal
-        isRegistered(msg.sender)
-        // isOwner(nftAddress, tokenId, creator)
+        isRegistered(msg.sender) // isOwner(nftAddress, tokenId, creator)
     {
         // add milestone NFTs, check first if the sender registered, is actually own the NFT
         // then check if not more than max milestones
@@ -165,11 +166,11 @@ contract MilestoneNFTv2 is ReentrancyGuard,ERC1155 {
             s_milestoneArray[msg.sender].length < MAX_MILESTONE,
             "You reach limit of milestone"
         );
-        console.log('is it go here?');
+        console.log("is it go here?");
         s_milestoneArray[msg.sender].push(
             Milestone(price, nftAddress, tokenId)
         );
-            console.log('reach here?');
+        console.log("reach here?");
     }
 
     /*
@@ -193,21 +194,22 @@ contract MilestoneNFTv2 is ReentrancyGuard,ERC1155 {
         return s_milestoneArray[creator].length < MAX_MILESTONE;
     }
 
-    function isCreator(address creatorAddress) internal view returns (bool) {
-        Creator memory creator = s_creators[creatorAddress];
+    function isCreator() public view returns (bool) {
+        Creator memory creator = s_creators[msg.sender];
         if (creator.addr == address(0)) {
             return false;
         }
         return true;
     }
 
-    function isEligibleToMint(address creatorAddress, address donator, uint256 milestoneId)
-        public
-        view
-        returns (bool)
-    {
+    function isEligibleToMint(
+        address creatorAddress,
+        address donator,
+        uint256 milestoneId
+    ) public view returns (bool) {
         if (
-            s_donators[donator][creatorAddress].status[milestoneId] == true && s_donators[donator][creatorAddress].minted[milestoneId] == false
+            s_donators[donator][creatorAddress].status[milestoneId] == true &&
+            s_donators[donator][creatorAddress].minted[milestoneId] == false
         ) {
             return true;
         }
@@ -221,6 +223,7 @@ contract MilestoneNFTv2 is ReentrancyGuard,ERC1155 {
     {
         return s_milestoneArray[creator][milestoneId].tokenId;
     }
+
     // pr disini update nya harus bener address nya
     function afterMintMilestone(address creator, uint256 milestoneId) internal {
         s_donators[msg.sender][creator].minted[milestoneId] = true;
@@ -234,65 +237,70 @@ contract MilestoneNFTv2 is ReentrancyGuard,ERC1155 {
         return s_creators[address(creator)];
     }
 
-      function _setTokenURI(uint256 tokenId, string memory _tokenURI)
+    function _setTokenURI(uint256 tokenId, string memory _tokenURI)
         internal
         virtual
     {
-        _tokenURIs[tokenId] = _tokenURI;
+        tokenURI[tokenId] = _tokenURI;
     }
 
     // mint as creator
     function mintCreatorNFT(string memory _tokenURI, uint256 _price)
-        public payable
+        public
+        payable
         returns (uint256)
     {
         // check if creator
-        require(
-            isCreator(msg.sender),
-            "Not a creator, register first"
-        );
+        require(isCreator(), "Not a creator, register first");
         // check if reach maximum milestone
-        require(
-            isLessMilestones(msg.sender),
-            "You reach limit of milestone"
-        );
+        require(isLessMilestones(msg.sender), "You reach limit of milestone");
         _tokenIds.increment();
         uint256 newItemId = _tokenIds.current();
         _mint(msg.sender, newItemId, 1, "");
         _setTokenURI(newItemId, _tokenURI);
         // after mint, please update CreatorMgnt NFTMilestone (call addMilestone)
-        addMilestone(
-            address(this),
-            newItemId,
-            _price
-        );
+        addMilestone(address(this), newItemId, _price);
         return newItemId;
     }
 
     // mint as a donator
     // TODO need to check whether elligible trough Mgmt contract
     function mintDonatorNFT(address artist, uint256 milestoneId)
-        public payable
+        public
+        payable
         returns (uint256)
     {
         // check if he eligible to mint the milestoneNFT?
         require(
-            isEligibleToMint(
-                artist,
-                msg.sender,
-                milestoneId
-            ),
+            isEligibleToMint(artist, msg.sender, milestoneId),
             "You are not elligible"
         );
         // need to check and not mint if already mint one
-        _mint(
-            msg.sender,
-            getTokenId(artist, milestoneId),
-            1,
-            ""
-        );
+        _mint(msg.sender, getTokenId(artist, milestoneId), 1, "");
         // set to not be able to mint again for this milestoneId
         afterMintMilestone(artist, milestoneId);
         return milestoneId;
+    }
+
+    function uri(uint256 _id) public view override returns (string memory) {
+        return tokenURI[_id];
+    }
+
+    function getMilestones(address creator)
+        public
+        view
+        returns (string[] memory)
+    {
+        uint256 milestoneCount = s_milestoneArray[creator].length;
+
+        string[] memory uris = new string[](milestoneCount);
+
+        if (milestoneCount > 0) {
+            for (uint256 i = 0; i < milestoneCount; i++) {
+                uris[i] = uri(s_milestoneArray[creator][i].tokenId);
+            }
+        }
+
+        return uris;
     }
 }
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,31 @@
 require("@nomicfoundation/hardhat-toolbox")
 require("hardhat-deploy")
+require("dotenv").config()
 
 /** @type import('hardhat/config').HardhatUserConfig */
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "0x"
+const BAOBAB_RPC_URL =
+    process.env.BAOBAB_RPC_URL || "https://api.baobab.klaytn.net:8651"
+
 module.exports = {
+    networks: {
+        hardhat: {
+            chainId: 31337,
+        },
+        localhost: {
+            chainId: 31337,
+        },
+        baobab: {
+            url: BAOBAB_RPC_URL,
+            accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+            //accounts: {
+            //     mnemonic: MNEMONIC,
+            // },
+            saveDeployments: true,
+            chainId: 1001,
+        },
+    },
     solidity: "0.8.17",
     namedAccounts: {
         deployer: 0,
